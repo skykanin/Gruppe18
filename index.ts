130e315f7cba54c41c65f8408916568259f9a7ba
skykanin <nicholas.jaunsen@gmail.com>
2017-09-14 10:50:32 +0200

    Set up index.js and added tsconfig file

--- index.ts
+++ index.ts
@@ -1,15 +1,21 @@
 class main {
-    constructor(tall, test) {
+    tall: number;
+    test: string;
+
+    constructor(tall: number, test: string){
         this.tall = tall;
         this.test = test;
     }
-    getTall() {
+
+    getTall(){
         return this.tall;
     }
-    getTest() {
+
+    getTest(){
         return this.test;
     }
 }
+
 var dette = new main(12, "Hello World");
 console.log(dette.getTall());
 console.log(dette.getTest());
