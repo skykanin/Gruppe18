01d5c8ca420a74369255c7208659c30be4d6c053
surGeonGG <sugeongg@gmail.com>
2017-09-15 06:17:42 +0200

    added som packages: pug, electron-reload fixed filestructure call loadPage() function i main.js for å navigere

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
+const remote = require('electron').remote
+const main = remote.require('./main.js')
+
+
 let img=document.getElementsByTagName("img")[0],
     trans=document.getElementById("trans");
 trans.onmouseenter=()=>{
@@ -6,3 +10,11 @@ trans.onmouseenter=()=>{
 trans.onmouseleave=()=>{
     img.style.filter="grayscale(100%) blur(10px)"
 }
+
+let manager = document.getElementById("nav").children[0]
+console.log(manager)
+
+manager.onclick = () => {
+    main.loadPage("manager.pug")
+}
+
---
+++ js/manager.js
@@ -0,0 +1,3 @@
+const remote = require('electron').remote
+const main = remote.require('./main.js')
+
