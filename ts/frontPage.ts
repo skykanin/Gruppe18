825f4a3964ab48eb5f714a0ade03f2f9e93fd799
skykanin <3789764+skykanin@users.noreply.github.com>
2017-09-21 21:13:11 +0200

    Finished design of frontPage navigation wheel

--- ts/frontPage.ts
+++ ts/frontPage.ts
@@ -5,12 +5,22 @@ const main = remote.require('./main.js');
 var nameList = ["arrangor.png", "bookingansvarlig.png", "bookingsjef.png","kontakt.png", "kundeservice.png", "lydtekniker.png", "lystekniker.png", "manager.png"];
 var listElement = document.getElementsByClassName('listIndex') as HTMLCollectionOf<HTMLElement>;
 var imgTags = document.getElementsByClassName('images') as HTMLCollectionOf<HTMLImageElement>;
+var links = document.getElementsByClassName('links') as HTMLCollectionOf<HTMLElement>;
+
+var centralAngle = 360/listElement.length;
 
 for(let i = 0; i < listElement.length; i++) {
-    listElement[i].style.transform = "rotate("+String(360/8*i)+"deg)";
-    listElement[i].style.transform += "skew("+String(90-360/8)+"deg)";
+    listElement[i].style.transform = "rotate("+String(centralAngle*i)+"deg)";
+    listElement[i].style.transform += "skew("+String(90-centralAngle)+"deg)";
+    listElement[i].style.backgroundColor = "hsl("+45*i+", 70%, 50%)";
+
+    links[i].style.transform = "skew("+String(-(90-centralAngle))+"deg)";
+    links[i].style.transform += "rotate("+String(-centralAngle*i)+"deg)";
 
     imgTags[i].src = "../images/menu icons/" + nameList[i];
-    imgTags[i].style.transform = "rotate("+String(-360/8*i)+"deg)";
-    imgTags[i].style.transform += "skew("+String(-90-360/8)+"deg)";
+
+    // imgTags[i].style.left = String(130*Math.cos(2*Math.PI*i/listElement.length)) + "px";
+    // imgTags[i].style.top = String(110*Math.cos(2*Math.PI*i/listElement.length)) + "px";
+    imgTags[i].id = "imgTag" + String(i);
+
 }
