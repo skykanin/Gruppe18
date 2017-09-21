7baec2ff8f8b925acbe4ab911d81866d5f4e54bb
skykanin <nicholas.jaunsen@gmail.com>
2017-09-21 14:12:46 +0200

    Added circle manually, still incomplete

--- js/frontPage.js
+++ js/frontPage.js
@@ -1,21 +1,13 @@
 //Don't touch the js version of this ts document
-/*const remote = require('electron').remote;
+const remote = require('electron').remote;
 const main = remote.require('./main.js');
-
-var buttons = document.getElementsByClassName('buttons') as HTMLCollectionOf<HTMLElement>;
-
-for(let i = 0; i < buttons.length; i++) {
-    buttons[i].style.left = Math.cos(2*Math.PI*i/buttons.length +1.5*Math.PI)*250 + 285 - 15 + "px";
-    buttons[i].style.top = Math.sin(2*Math.PI*i/buttons.length +1.5*Math.PI)*250 + 300 - 30 + "px";
-}*/
-wheel = new wheelnav("wheelDiv", null, 600, 600);
-wheel.slicePathFunction = slicePath().DonutSlice;
-wheel.markerPathFunction = markerPath().PieLineMarker;
-wheel.clickModeRotate = false;
-wheel.colors = colorpalette.fractallove;
-wheel.markerEnable = true;
-wheel.createWheel(["1", "2", "3", "4", "5", "6", "7", "8"]);
-wheel.sliceHoverAttr = { stroke: '#000000', 'stroke-width': 4 };
-wheel.lineHoverAttr = { stroke: '#000000', 'stroke-width': 3 };
-wheel.titleHoverAttr = { fill: '#000000', stroke: 'none' };
-wheel.refreshWheel();
+var nameList = ["arrangor.png", "bookingansvarlig.png", "bookingsjef.png", "kontakt.png", "kundeservice.png", "lydtekniker.png", "lystekniker.png", "manager.png"];
+var listElement = document.getElementsByClassName('listIndex');
+var imgTags = document.getElementsByClassName('images');
+for (let i = 0; i < listElement.length; i++) {
+    listElement[i].style.transform = "rotate(" + String(360 / 8 * i) + "deg)";
+    listElement[i].style.transform += "skew(" + String(90 - 360 / 8) + "deg)";
+    imgTags[i].src = "../images/menu icons/" + nameList[i];
+    imgTags[i].style.transform = "rotate(" + String(-360 / 8 * i) + "deg)";
+    imgTags[i].style.transform += "skew(" + String(-90 - 360 / 8) + "deg)";
+}
