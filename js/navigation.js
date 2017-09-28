ddab5335f10550fb0280e3d15ba7d004eb1cc85c
skykanin <nicholas.jaunsen@gmail.com>
2017-09-28 10:12:50 +0200

    Fixed nav.js issue

--- js/navigation.js
+++ js/navigation.js
@@ -1,10 +1,4 @@
-let home = document.getElementById("home"),
-    manager = document.getElementById("man"),
-    bookchief = document.getElementById("bookchief"),
-    bookresp = document.getElementById("bookresp"),
-    arranger = document.getElementById("arr"),
-    technician = document.getElementById("tech"),
-    close = document.getElementById("close"),
+let close = document.getElementById("close"),
     maximize = document.getElementById("maximize"),
     minimize = document.getElementById("minimize"),
     nav = document.getElementById("nav");
@@ -28,29 +22,4 @@ maximize.onclick = () => {
     } else {
         window.unmaximize();
     }
-}
-
-
-home.onclick = () => {
-    main.loadPage("index.pug")
-}
-
-manager.onclick = () => {
-    main.loadPage("man-main.pug")
-}
-
-arranger.onclick = () => {
-    main.loadPage("arr-main.pug")
-}
-
-bookresp.onclick = () => {
-    main.loadPage("bookresp-main.pug")
-}
-
-bookchief.onclick = () => {
-    main.loadPage("bookchief-main.pug")
-}
-
-technician.onclick = () => {
-    main.loadPage("tech-main.pug")
 }
