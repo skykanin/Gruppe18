f24d6cab49dc686fbd82438522dd14e97b2b2bb0
skykanin <3789764+skykanin@users.noreply.github.com>
2017-10-01 16:34:58 +0200

    Added a register page and added logic for form validation for the log in page

--- js/loginPage.js
+++ js/loginPage.js
@@ -1,2 +1,38 @@
-//Don't touch the js version of this ts document
-var s = 7;
+//Don't edit the js version of this ts document
+class Login {
+    constructor() {
+        this.usernameField = document.getElementById('username');
+        this.passwordField = document.getElementById('password');
+        this.errorField = document.getElementById('errorMessage');
+        this.loginButton = document.getElementById('loginButton');
+        this.loginButton.addEventListener("click", () => {
+            this.readLoginInfo();
+            return false; //Prevents page from reloading
+        });
+    }
+    readLoginInfo() {
+        if (this.usernameField.value != null && this.usernameField.value != undefined && this.usernameField.value != '' && this.passwordField.value != null && this.passwordField.value != undefined && this.passwordField.value != '') {
+            this.username = this.usernameField.value;
+            this.password = this.passwordField.value;
+            console.log(this.username);
+            console.log(this.password);
+            this.errorField.innerHTML = '';
+        }
+        else if (this.usernameField.value != null && this.usernameField.value != undefined && this.usernameField.value != '') {
+            this.username = this.usernameField.value;
+            console.log(this.username);
+            this.errorField.innerHTML = 'Missing password';
+        }
+        else if (this.passwordField.value != null && this.passwordField.value != undefined && this.passwordField.value != '') {
+            this.password = this.passwordField.value;
+            console.log(this.password);
+            this.errorField.innerHTML = 'Missing username';
+        }
+        else {
+            this.errorField.innerHTML = 'Missing username and password';
+        }
+    }
+}
+window.onload = () => {
+    let login = new Login();
+};
--- ts/loginPage.ts
+++ ts/loginPage.ts
@@ -1,2 +1,43 @@
-//Don't touch the js version of this ts document
-var s = 7;
+//Don't edit the js version of this ts document
+class Login {
+    usernameField = document.getElementById('username') as HTMLInputElement;
+    passwordField = document.getElementById('password') as HTMLInputElement;
+    errorField = document.getElementById('errorMessage') as HTMLElement;
+    loginButton = document.getElementById('loginButton') as HTMLButtonElement;
+    username: string;
+    password: string;
+
+    constructor() {
+        this.loginButton.addEventListener("click", () => {
+            this.readLoginInfo();
+            return false; //Prevents page from reloading
+        });
+    }
+
+    readLoginInfo() {
+        if (this.usernameField.value != null && this.usernameField.value != undefined && this.usernameField.value != '' && this.passwordField.value != null && this.passwordField.value != undefined && this.passwordField.value != '') {
+            this.username = this.usernameField.value;
+            this.password = this.passwordField.value;
+            console.log(this.username);
+            console.log(this.password);
+            this.errorField.innerHTML = '';
+        }
+        else if (this.usernameField.value != null && this.usernameField.value != undefined && this.usernameField.value != '') {
+            this.username = this.usernameField.value;
+            console.log(this.username);
+            this.errorField.innerHTML = 'Missing password';
+        }
+        else if (this.passwordField.value != null && this.passwordField.value != undefined && this.passwordField.value != '') {
+            this.password = this.passwordField.value;
+            console.log(this.password);
+            this.errorField.innerHTML = 'Missing username';
+        }
+        else {
+            this.errorField.innerHTML = 'Missing username and password';
+        }
+    }
+}
+
+window.onload = () => {
+    let login = new Login();
+}
