5774508c8d4c5ce2369ad1b6fcd6df5169735ee2
skykanin <nicholas.jaunsen@gmail.com>
2017-10-04 11:08:27 +0200

    Finished login check for login page

--- js/loginPage.js
+++ js/loginPage.js
@@ -1,37 +1,77 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 //Don't edit the js version of this ts document
-const mysql = require('mysql');
+const mysql = require("mysql");
+const passwordHAS = require("password-hash-and-salt");
 class Login {
     constructor() {
         this.usernameField = document.getElementById('username');
         this.passwordField = document.getElementById('password');
         this.errorField = document.getElementById('errorMessage');
         this.loginButton = document.getElementById('loginButton');
-        this.loginButton.addEventListener("click", () => {
-            this.validateForm();
-            return false; //Prevents page from reloading
+        this.connection = mysql.createConnection({
+            host: 'mysql.stud.ntnu.no',
+            user: 'andrris_gruppe18',
+            password: 'cdji2005',
+            database: 'andrris_gruppe18',
+        });
+        this.loginButton.addEventListener("click", (e) => {
+            e.preventDefault(); //Prevents page from reloading
+            if (this.validateForm()) {
+                this.checkUsernameExists();
+            }
         });
     }
     validateForm() {
-        if (this.usernameField.value != null && this.usernameField.value != '' && this.passwordField.value != null && this.passwordField.value != '') {
+        if (this.usernameField.value != '' && this.passwordField.value != '') {
             this.username = this.usernameField.value;
             this.password = this.passwordField.value;
-            console.log(this.username);
-            console.log(this.password);
             this.errorField.innerHTML = '';
+            return true;
         }
-        else if (this.usernameField.value != null && this.usernameField.value != '') {
+        else if (this.usernameField.value != '') {
             this.username = this.usernameField.value;
             console.log(this.username);
             this.errorField.innerHTML = 'Missing password';
+            return false;
         }
-        else if (this.passwordField.value != null && this.passwordField.value != '') {
+        else if (this.passwordField.value != '') {
             this.password = this.passwordField.value;
             console.log(this.password);
             this.errorField.innerHTML = 'Missing username';
+            return false;
         }
         else {
             this.errorField.innerHTML = 'Missing username and password';
+            return false;
         }
+    }
+    checkUsernameExists() {
+        this.connection.query('SELECT * from USER', (err, result) => {
+            if (err) {
+                console.log(err);
+            }
+            for (let i = 0; i < result.length; i++) {
+                if (this.username == result[i].username) {
+                    this.errorField.innerHTML = "";
+                    this.checkPassword(result[i].password);
+                }
+            }
+        });
+        this.errorField.innerHTML = "Username doesn't exist";
+    }
+    checkPassword(databasePassword) {
+        passwordHAS(this.password).verifyAgainst(databasePassword, (error, verified) => {
+            if (error)
+                throw new Error('Something went wrong!');
+            if (!verified) {
+                this.errorField.innerHTML = "Incorrect password";
+                throw new Error('Incorrect password');
+            }
+            else {
+                window.location.href = "../html/frontpage.pug";
+            }
+        });
     }
 }
 window.onload = () => {
--- ts/loginPage.ts
+++ ts/loginPage.ts
@@ -1,42 +1,83 @@
 //Don't edit the js version of this ts document
-const mysql = require('mysql');
+import * as mysql from 'mysql';
+import * as passwordHAS from 'password-hash-and-salt';
 
 class Login {
     usernameField = document.getElementById('username') as HTMLInputElement;
     passwordField = document.getElementById('password') as HTMLInputElement;
     errorField = document.getElementById('errorMessage') as HTMLElement;
     loginButton = document.getElementById('loginButton') as HTMLButtonElement;
+    connection: mysql.IConnection;
     username: string;
     password: string;
+    databasePassword: string;
 
     constructor() {
-        this.loginButton.addEventListener("click", () => {
-            this.validateForm();
-            return false; //Prevents page from reloading
+        this.connection = mysql.createConnection({
+            host: 'mysql.stud.ntnu.no',
+            user: 'andrris_gruppe18',
+            password: 'cdji2005',
+            database: 'andrris_gruppe18',
+        });
+        this.loginButton.addEventListener("click", (e: any) => {
+            e.preventDefault(); //Prevents page from reloading
+            if(this.validateForm()) {
+                this.checkUsernameExists();
+            }
         });
     }
 
-    validateForm():void {
-        if (this.usernameField.value != null && this.usernameField.value != '' && this.passwordField.value != null && this.passwordField.value != '') {
+    validateForm():boolean {
+        if (this.usernameField.value != '' && this.passwordField.value != '') {
             this.username = this.usernameField.value;
             this.password = this.passwordField.value;
-            console.log(this.username);
-            console.log(this.password);
             this.errorField.innerHTML = '';
+            return true;
         }
-        else if (this.usernameField.value != null && this.usernameField.value != '') {
+        else if (this.usernameField.value != '') {
             this.username = this.usernameField.value;
             console.log(this.username);
             this.errorField.innerHTML = 'Missing password';
+            return false;
         }
-        else if (this.passwordField.value != null && this.passwordField.value != '') {
+        else if (this.passwordField.value != '') {
             this.password = this.passwordField.value;
             console.log(this.password);
             this.errorField.innerHTML = 'Missing username';
+            return false;
         }
         else {
             this.errorField.innerHTML = 'Missing username and password';
+            return false;
         }
+    }
+
+    checkUsernameExists():void {
+        this.connection.query('SELECT * from USER', (err, result) => {
+            if(err) {
+                console.log(err);
+            }
+            for(let i = 0; i < result.length; i++) {
+                if(this.username == result[i].username) { //checks if username exists in database
+                    this.errorField.innerHTML = "";
+                    this.checkPassword(result[i].password);
+                }
+            }
+        });
+        this.errorField.innerHTML = "Username doesn't exist";
+    }
+
+    checkPassword(databasePassword: string):void {
+        passwordHAS(this.password).verifyAgainst(databasePassword, (error, verified) => {
+            if(error)
+                throw new Error('Something went wrong!');
+            if(!verified) {
+                this.errorField.innerHTML = "Incorrect password";
+                throw new Error('Incorrect password');
+            } else {
+                window.location.href = "../html/frontpage.pug";
+            }
+        });
     }
 }
 
